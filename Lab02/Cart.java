a00aa18ac916cb9ba2e882d6797af410008cd925
Bminh2005 <binhminh4102005@gmail.com>
2025-03-26 07:43:18 +0700

    7:43 26/3/2025

--- Lab02/Cart.java
+++ Lab02/Cart.java
@@ -20,7 +20,15 @@ public class Cart {
 			System.out.println("Adding disc into your cart is FAIL!");
 		}
 	}
-	
+	public void addDigitalVideoDisc (DigitalVideoDisc[] disc) {
+		for (int i = 0; i < disc.length; i++) {
+			addDigitalVideoDisc(disc[i]);
+		}
+	}
+	public void addDigitalVideoDisc (DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+		addDigitalVideoDisc(dvd1);
+		addDigitalVideoDisc(dvd2);
+	}
 	public void removeDigitalVideoDisc (DigitalVideoDisc disc) {
 		for(int i = 0; i < qtyOrdered; i++) {
 			if(itemsOrdered[i].equals(disc)) {
@@ -45,5 +53,12 @@ public class Cart {
 	
 	public void printTotalCost() {
 		System.out.println("TOTAL COST: $" + this.totalCost());
+	}
+	
+	public void displayCart() {
+		for(int i = 0; i < qtyOrdered; i++) {
+			System.out.printf("%-2d\t%-20s\t$%4.2f\n", i+1, this.itemsOrdered[i].getTitle(), this.itemsOrdered[i].getCost());
+		}
+		System.out.printf("%2s\t%-20s\t$%4.2f\n", "", "Total Cost", this.totalCost());
 	}
 }
--- Lab02/DigitalVideoDisc.java
+++ Lab02/DigitalVideoDisc.java
@@ -6,21 +6,35 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	private static int nbDigitalVideoDiscs = 0;
+	private int id;
 	
 	public DigitalVideoDisc() {
 		super();
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
 	}
 		
+	public void setTitle(String title) {
+		this.title = title;
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
+	}
+
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
@@ -29,6 +43,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -38,6 +54,12 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		DigitalVideoDisc.nbDigitalVideoDiscs++;
+		this.id = DigitalVideoDisc.nbDigitalVideoDiscs;
+	}
+	
+	public int getId() {
+		return this.id;
 	}
 
 	public String getTitle() {
